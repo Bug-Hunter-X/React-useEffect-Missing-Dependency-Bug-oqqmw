f57a9c6681976662dc87c426c7d9e48febdd8041
BugHunter-X <askzenai@gmail.com>
2025-01-17 17:26:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect dependency array
+    const interval = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []); // Missing 'count' dependency
+
+  return <div>Count: {count}</div>;
+}
+```
